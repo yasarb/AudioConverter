267d446021a78336edb07841d485fb224d9110ed
yasar budulgan <yb.budulgan@gmail.com>
2017-06-10 00:52:04 +0300

    Modify spring context. Add a temp code for spring beans

--- ac-app/src/main/java/com/ysrbdlgn/audioconverter/app/MainApp.java
+++ ac-app/src/main/java/com/ysrbdlgn/audioconverter/app/MainApp.java
@@ -1,11 +1,22 @@
 package com.ysrbdlgn.audioconverter.app;
 
+import com.ysrbdlgn.audioconverter.common.CommonTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class MainApp {
+
+
     public static void main( String[] args ) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"/spring/application-context-app.xml"});
+
+        CommonTest comm = (CommonTest) applicationContext.getBean("commonTestBean");
+        comm.display();
 
 
     }
