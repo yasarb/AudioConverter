a620d2a1575a69ae88b1221748f704776bf79bf8
yasar budulgan <yb.budulgan@gmail.com>
2017-06-11 15:03:23 +0300

    Implement browse button action

--- ac-frontend/src/main/java/com/ysrbdlgn/audioconverter/frontend/ui/controller/MainSceneControllerImpl.java
+++ ac-frontend/src/main/java/com/ysrbdlgn/audioconverter/frontend/ui/controller/MainSceneControllerImpl.java
@@ -1,30 +1,38 @@
 package com.ysrbdlgn.audioconverter.frontend.ui.controller;
 
+import com.ysrbdlgn.audioconverter.frontend.AudioConverterApplication;
 import com.ysrbdlgn.audioconverter.frontend.ui.FileTable;
 import com.ysrbdlgn.audioconverter.frontend.ui.model.FileTableEntry;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.DirectoryChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * Created by ysrbdlgn on 10-Jun-17.
+ * Created by ysrbdlgn on 11-Jun-17.
  */
-public class MainAppController implements Initializable {
+public class MainSceneControllerImpl implements MainSceneController {
 
     @FXML private FileTable fileTable;
+    @FXML private TextField outputPathField;
     @FXML private TableColumn<FileTableEntry, Integer> colID;
     @FXML private TableColumn<FileTableEntry, String> colFileName;
     @FXML private TableColumn<FileTableEntry, String> colTitle;
     @FXML private TableColumn<FileTableEntry, String> colDuration;
     @FXML private TableColumn<FileTableEntry, String> colState;
 
+    private AudioConverterApplication application;
     private ObservableList<FileTableEntry> fileTableEntries;
+
+    public MainSceneControllerImpl() {}
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -38,10 +46,26 @@ public class MainAppController implements Initializable {
 
 
         fileTableEntries = FXCollections.observableArrayList(
-                new FileTableEntry(1, "C:", "dosya1", 126513)
+                new FileTableEntry(1, "C:", "dosya1", 126513),
+                new FileTableEntry(2, "C:\\Program Files\\", "program files", 8451136)
         );
 
         fileTable.getItems().setAll(fileTableEntries);
 
     }
+
+    @FXML
+    public void browseButtonPressed(ActionEvent actionEvent) {
+
+        DirectoryChooser chooser = new DirectoryChooser();
+        /* TODO: Initial directory should be the directory where app started.*/
+        chooser.setInitialDirectory(new File("C:\\"));
+        File directory = chooser.showDialog(AudioConverterApplication.getScene().getWindow());
+
+        if(directory == null)
+            return;
+
+        outputPathField.setText(directory.getAbsolutePath());
+    }
+
 }
