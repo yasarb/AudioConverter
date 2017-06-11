e0d78714f6e8decc674784b4c924fae7858fbee6
yasar budulgan <yb.budulgan@gmail.com>
2017-06-11 17:08:58 +0300

    Add ribbon menu controller. Implement "Add file" button action.

---
+++ ac-frontend/src/main/java/com/ysrbdlgn/audioconverter/frontend/ui/controller/RibbonMenuController.java
@@ -0,0 +1,10 @@
+package com.ysrbdlgn.audioconverter.frontend.ui.controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by ysrbdlgn on 11-Jun-17.
+ */
+public interface RibbonMenuController {
+    void addFileButtonPressed(ActionEvent actionEvent);
+}
---
+++ ac-frontend/src/main/java/com/ysrbdlgn/audioconverter/frontend/ui/controller/RibbonMenuControllerImpl.java
@@ -0,0 +1,62 @@
+package com.ysrbdlgn.audioconverter.frontend.ui.controller;
+
+import com.ysrbdlgn.audioconverter.common.entity.EFileType;
+import com.ysrbdlgn.audioconverter.frontend.AudioConverterApplication;
+import com.ysrbdlgn.audioconverter.frontend.service.FileTableService;
+import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ysrbdlgn on 11-Jun-17.
+ */
+public class RibbonMenuControllerImpl implements RibbonMenuController {
+
+    private FileTableService fileTableService;
+
+    @Override
+    public void addFileButtonPressed(ActionEvent actionEvent) {
+
+        FileChooser chooser = new FileChooser();
+        /* TODO: Initial directory should be the directory where app started.*/
+
+        chooser.setInitialDirectory(new File("C:\\"));
+        addExtensionFilters(chooser);
+        File file = chooser.showOpenDialog(AudioConverterApplication.getScene().getWindow());
+
+        if(file == null)
+            return;
+
+        fileTableService.addFile(file);
+
+    }
+
+    private void addExtensionFilters(FileChooser chooser){
+
+        String extensionTemplate = "*.%s";
+
+        // Add individual filters
+        for(EFileType fileType : EFileType.values()) {
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                    fileType.getDescription(), String.format(extensionTemplate, fileType.getText()));
+            chooser.getExtensionFilters().add(extFilter);
+        }
+
+        // Add "all audio files" item
+
+        List<String> allExtensions = new ArrayList<>();
+        for(EFileType fileType : EFileType.values()) {
+            allExtensions.add(String.format(extensionTemplate, fileType.getText()));
+        }
+        FileChooser.ExtensionFilter allExtFilter = new FileChooser.ExtensionFilter("All Audio Files", allExtensions);
+        chooser.getExtensionFilters().add(0, allExtFilter);
+
+    }
+
+    public void setFileTableService(FileTableService fileTableService) {
+        this.fileTableService = fileTableService;
+    }
+}
